e24655a55d7253d4b59efe99067acf0f76da340c
Giovanna Alves <giovannabadaroalves@gmail.com>
2021-10-09 16:54:00 -0300

    🔧 config(project | client): add eslint, prettier

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,43 +1,33 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index.bundle.js",
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
   },
   devServer: {
     hot: true,
     port: 3000,
     open: true,
-
   },
   module: {
     rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use:{
-                loader: "babel-loader",
-            }
-            
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-            test: /\.scss$/,
-            use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-            
-        }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   resolve: {
-    modules: [__dirname, "src", "node_modules"],
-    extensions: ["*", ".js", ".jsx", ".json"],
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.json'],
   },
-
-
-
 };
