dadf067097d920ffac75c857276a3f362bb8d7d4
Giovanna Alves <giovannabadaroalves@gmail.com>
2021-10-13 18:01:01 -0300

    🚀  deploy stuff

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:3333/api' })
+const api = axios.create({
+  baseURL: 'https://search-box-bff.herokuapp.com/api',
+})
 
 export default api
